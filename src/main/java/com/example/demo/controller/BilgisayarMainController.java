8f07f8d90f8b9990e0af480cc03c712379ec168b
nursultanKanybekov <nursultankanybekov12082000@gmail.com>
2024-03-25 22:38:49 -0700

    database part added

--- src/main/java/com/example/demo/controller/BilgisayarMainController.java
+++ src/main/java/com/example/demo/controller/BilgisayarMainController.java
@@ -1,20 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.MainModel;
+import com.example.demo.repo.MainRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BilgisayarMainController {
+    @Autowired
+    private MainRepo mainRepo;
 
-    @GetMapping ("/main")
-    public String mainPageG(){
+    @GetMapping("/main")
+    public String mainPageG() {
         return "index";
     }
 
     @PostMapping("/")
-    public String mainPageP(String login, String password){
-        System.out.println(login+" "+password);
+    public String mainPageP(String login, String password) {
+        mainRepo.save(new MainModel(login, password));
         return "index";
     }
 
